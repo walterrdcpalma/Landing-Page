38d3a9f6d747ac58ebfdfc705963e29d274b9588
Walter Palma <walterrdcpalma@gmail.com>
2020-06-15 01:47:00 +0100

    fix2 scroll

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ const navSlide = () => {
   const nav = document.querySelector(".nav-links");
 
   burger.addEventListener("click", () => {
-      nav.classList.toggle("nav-active");
-      
-  })
+    nav.classList.toggle("nav-active");
+  });
 };
-
-
 
 navSlide();
